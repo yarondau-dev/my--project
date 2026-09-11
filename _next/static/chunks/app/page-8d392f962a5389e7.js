3d36fd627f17dd2c294b977649a67b654dbb6070
yarondau-dev <yarondau-dev@users.noreply.github.com>
2026-09-11 12:51:15 +0000

    Updates

---
+++ _next/static/chunks/app/page-8d392f962a5389e7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[307,351,605,974],{8007:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,2619,23)),Promise.resolve().then(n.t.bind(n,1356,23))}},e=>{e.O(0,[356,619,441,255,358],()=>e(e.s=8007)),_N_E=e.O()}]);
